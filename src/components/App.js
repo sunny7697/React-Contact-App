d44a0d28757ced8cd555c4886ae1c4547ff778dc
Thakur <sunny.mca19.du@gmail.com>
2021-06-03 13:10:01 +0530

    json-server set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {uuid} from 'uuidv4';
+import api from '../api/contacts';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact'
@@ -10,6 +11,12 @@ import ContactDetails from './ContactDetails'
 function App() {
   const Local_Storage_Key = "contacts";
   const [contacts, setContacts] = useState([]);
+
+  // Retrieve contacts from json-server
+  const retrieveContacts = async () => {
+      const response = await api.get('/contacts');
+      return response.data;
+  }
 
   // add contact in the contact list
   const addContactHandler = contact => {
@@ -28,10 +35,15 @@ function App() {
 
   // after every page refresh or load, get the contacts stored in the local storage
   useEffect(() => {
-    const retrieveContacts = JSON.parse(localStorage.getItem(Local_Storage_Key));
-    if(retrieveContacts) {
-      setContacts(retrieveContacts);
+    // const retrieveContacts = JSON.parse(localStorage.getItem(Local_Storage_Key));
+    // if(retrieveContacts) setContacts(retrieveContacts);
+
+    const getAllContacts = async () => {
+      const allContacts = await retrieveContacts();
+      if(allContacts) setContacts(allContacts);
     }
+
+    getAllContacts();
   }, [])
 
   // after every contacts addition, contact added in the local storage
